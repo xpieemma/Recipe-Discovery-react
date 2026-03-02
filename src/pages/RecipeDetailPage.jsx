898eb9c1a876b4b1101a00f9382dfdebd0688930
E. Pierre <7chrispeter@gmail.com>
2026-03-02 00:06:57 -0500

    image, category and area shows with  http://localhost:5173/recipe/52772

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -1,17 +1,18 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import Spinner from "../components/Spinner";
 import ErrorMessage from "../components/ErrorMessage";
 
-
 export default function RecipeDetailPage() {
-  const {recipeId} = useParams();
+  const { recipeId } = useParams();
 
-  const {data, loading, error} = useFetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
-if (loading) return <Spinner/>
-if (error) return <ErrorMessage message={error} />
-const recipe = data?.meals?.[0];
-if(!recipe) return <ErrorMessage message="cant be found" />
+  const { data, loading, error } = useFetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`,
+  );
+  if (loading) return <Spinner />;
+  if (error) return <ErrorMessage message={error} />;
+  const recipe = data?.meals?.[0];
+  if (!recipe) return <ErrorMessage message="cant be found" />;
 
   console.log(recipeId);
 
@@ -19,9 +20,14 @@ if(!recipe) return <ErrorMessage message="cant be found" />
 
   return (
     <div className="recipe-detail">
-      <h1>
-{recipe.strMeal} loaded successfully 
-      </h1>
+      <div className="recipe-header">
+        <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+        <div className="recipe-info">
+          <h1>{recipe.strMeal}</h1>
+          <p className="category-tag"> {recipe.strCategory}</p>
+          {recipe.strArea && <p className="area-tag">{recipe.strArea}</p>}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
