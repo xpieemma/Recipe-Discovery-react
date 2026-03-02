a9abf8685dab72d58a725ab6ff992edd9089dc50
E. Pierre <7chrispeter@gmail.com>
2026-03-01 23:05:53 -0500

    fetching category data from api

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom"
+import useFetch from "../hooks/useFetch";
 
 
 export default function CategoryPage() {
   const {categoryName} = useParams();
+  const {data} = useFetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c={categoryName}`);
 
 
   return (
 <div className="category-page">
   <h1>{categoryName} Recipes</h1>
+  {data && <pre> {JSON.stringify(data, null, 2)}</pre>}
 </div>
   );
 
