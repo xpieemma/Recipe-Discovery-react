dbd480c85861082c32f5f27ba5a581b234a9a750
E. Pierre <7chrispeter@gmail.com>
2026-03-01 23:23:45 -0500

    implement the card setup

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,16 +1,23 @@
 import { useParams } from "react-router-dom"
 import useFetch from "../hooks/useFetch";
+import ErrorMessage from "../components/ErrorMessage";
+import RecipeCard from "../components/RecipeCard";
 
 
 export default function CategoryPage() {
   const {categoryName} = useParams();
-  const {data} = useFetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c={categoryName}`);
-
+  const {data, loading, error} = useFetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c={categoryName}`);
+if (loading) return <Spinner />;
+if (error) return <ErrorMessage message={error}/>
+const recipes = data?.meals || [];
 
   return (
 <div className="category-page">
   <h1>{categoryName} Recipes</h1>
-  {data && <pre> {JSON.stringify(data, null, 2)}</pre>}
+  <div className="recipes-grid">
+    {recipes.map(recipe => <RecipeCard key={recipe.idMeal} recipe={recipe}/>)}
+
+  </div>
 </div>
   );
 
