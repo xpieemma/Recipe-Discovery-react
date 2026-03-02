254341db1c4241fc6001f0c719907c5953407c73
E. Pierre <7javastudent@gmail.com>
2026-03-01 22:45:02 -0500

    CategoryPage and API_URL params

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,14 +1,14 @@
+import { useParams } from "react-router-dom"
 
 
 export default function CategoryPage() {
+  const {categoryName} = useParams();
+
 
   return (
-
-    <>
-   
-  
-    
-    </>
-  )
+<div className="category-page">
+  <h1>{categoryName} Recipes</h1>
+</div>
+  );
 
 }
