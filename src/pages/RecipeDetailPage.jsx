d21d91595998a85ef4d3936ad363cfff2528b882
E. Pierre <7chrispeter@gmail.com>
2026-03-01 23:59:20 -0500

    http://localhost:5173/recipe/52772 provided the attribute recipe

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -1,15 +1,26 @@
 import { useParams } from "react-router-dom"
+import useFetch from "../hooks/useFetch";
+import Spinner from "../components/Spinner";
+import ErrorMessage from "../components/ErrorMessage";
 
 
 export default function RecipeDetailPage() {
   const {recipeId} = useParams();
 
-  
-  
+  const {data, loading, error} = useFetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+if (loading) return <Spinner/>
+if (error) return <ErrorMessage message={error} />
+const recipe = data?.meals?.[0];
+if(!recipe) return <ErrorMessage message="cant be found" />
+
+  console.log(recipeId);
+
+  console.log(data);
+
   return (
     <div className="recipe-detail">
       <h1>
-loading Recipe {recipeId}...
+{recipe.strMeal} loaded successfully 
       </h1>
     </div>
   )
