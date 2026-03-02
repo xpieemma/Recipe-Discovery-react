801046d6e70880287ac90907b2e3d93a62ac75ea
E. Pierre <7chrispeter@gmail.com>
2026-03-01 23:43:05 -0500

    http://localhost:5173/recipe/52772 ... shows loading Recipe 52772...

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -1,10 +1,16 @@
+import { useParams } from "react-router-dom"
 
 
 export default function RecipeDetailPage() {
+  const {recipeId} = useParams();
+
+  
   
   return (
-    <>
-
-    </>
+    <div className="recipe-detail">
+      <h1>
+loading Recipe {recipeId}...
+      </h1>
+    </div>
   )
 }
