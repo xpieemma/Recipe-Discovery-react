b369b2455e2c61145018b04fb5bf68efc710c5f6
E. Pierre <7chrispeter@gmail.com>
2026-03-02 00:25:29 -0500

    ingredient list and instructions

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -12,12 +12,16 @@ export default function RecipeDetailPage() {
   if (loading) return <Spinner />;
   if (error) return <ErrorMessage message={error} />;
   const recipe = data?.meals?.[0];
-  if (!recipe) return <ErrorMessage message="cant be found" />;
+  if (!recipe) return <ErrorMessage message="Recipe not found" />;
 
-  console.log(recipeId);
-
-  console.log(data);
-
+const ingredients = [];
+for (let i = 1; i<=20; i++){ // loop to read all 20 
+  const ingredient = recipe[`strIngredient${i}`]; // 20 possible ingredient field
+  const measure = recipe [`strMeasure${i}`];
+  if (ingredient && ingredient.trim()){
+    ingredient.push({ingredient, measure});
+  }
+}
   return (
     <div className="recipe-detail">
       <div className="recipe-header">
@@ -28,6 +32,23 @@ export default function RecipeDetailPage() {
           {recipe.strArea && <p className="area-tag">{recipe.strArea}</p>}
         </div>
       </div>
+<div className="recipe-content">
+  <div className="ingredient-section">
+    <h2>Ingredients</h2>
+    <ul className="ingredients-list">
+      {ingredients.map((item, index) => ( <li key={index}>
+        <span className="measure">{item.measure}</span>
+        <span className="ingredient">{item.ingredient}</span>
+      </li>))}
+    </ul>
+  </div>
+  <div className="instructions-section">
+    <h2>Instructions</h2>
+    <p className="instructions">{recipe.strInstructions}</p>
+  </div>
+</div>
+
+
     </div>
   );
 }
